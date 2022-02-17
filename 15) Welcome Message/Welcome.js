8b2bf84f0afe29d8313b7bf81504c2675b9b79ea
Abhishek <abhishekkurkute869@gmail.com>
2022-02-17 10:55:51 +0530

    jquery added

---
+++ 15) Welcome Message/Welcome.js
@@ -0,0 +1,7 @@
+//Write Your jQuery Code Here
+//Write Your jQuery Code Here
+$("#btnId").click(function(){
+    var name=$('#txt').val();
+    var msg="Welcome "+name+"!";
+    $("#address").html('"'+msg+'"');
+});
